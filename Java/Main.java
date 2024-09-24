94d6d0ea491672e42a34c1c8c4b769164033c959
Davi Moreira <tcggamesd@gmail.com>
2024-09-24 18:34:10 -0300

    oadpkasdkpsa

---
+++ Java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        NumeroPrimo t = new NumeroPrimo();
+        t.verificarNumeroPrimo();
+    }
+}
--- Java/NumeroPrimo.java
+++ Java/NumeroPrimo.java
@@ -1,30 +1,26 @@
-public class NumeroPrimo {
-    public static void main(String[] args) {
-        int numero = 29; 
 
-        if (isPrimo(numero)) {
-            System.out.println(numero + " é um número primo.");
-        } else {
-            System.out.println(numero + " não é um número primo.");
-        }
-    }
 
-    private static boolean isPrimo(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        if (n == 2) {
-            return true;
-        }
-        if (n % 2 == 0) {
-            return false;
-        }
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
+import java.util.Scanner;
+
+public class NumeroPrimo{
+    Scanner prompt = new Scanner(System.in);
+    int a;
+    public void verificarNumeroPrimo(){
+        System.out.println("Digite um numero: ");
+        a = prompt.nextInt();
+        if(a != 1){
+            if(a%2 != 0 || a == 2){
+                if(a%1==0){
+                    if(a%a==0){
+                        System.out.println("O número " + a + " é primo");
+                    }
+                }
+            }else{
+                System.out.println("O número " + a + " não é primo");
             }
+        }else{
+            System.out.println("O número " + a + " não é primo");
         }
-        return true;
     }
-}
 
+}
